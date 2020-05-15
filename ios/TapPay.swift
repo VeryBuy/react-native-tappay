298b842cd26a9eb2c17675ee5530bd4cd0729eb6
Bingo Yang <blackbing@gmail.com>
2020-05-15 14:44:40 +0800

    fix attr naming

--- ios/TapPay.swift
+++ ios/TapPay.swift
@@ -116,12 +116,12 @@ class TapPay: NSObject {
                 {
                     resolve([
                         "prime": directPayPrime,
-                        "binCode": creditCardInfo.bincode ?? "",
-                        "lastFour": creditCardInfo.lastFour ?? "",
+                        "bincode": creditCardInfo.bincode ?? "",
+                        "lastfour": creditCardInfo.lastFour ?? "",
                         "issuer": creditCardInfo.issuer ?? "",
-                        "cardType": creditCardInfo.cardType,
+                        "cardtype": creditCardInfo.cardType,
                         "funding": creditCardInfo.funding,
-                        "cardIdentifier": creditCardIdentifier
+                        "cardidentifier": creditCardIdentifier
                     ])
                 }
             }.onFailureCallback { (status, message) in
