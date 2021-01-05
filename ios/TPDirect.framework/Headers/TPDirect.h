60d162d3109dc652c37cfb34dcad6706836c7995
mike <yum650350@gmail.com>
2021-01-05 11:35:02 +0800

    bump tappay ios version to 2.3.1

---
+++ ios/TPDirect.framework/Headers/TPDirect.h
@@ -0,0 +1,34 @@
+//
+//  TPDirect.h
+//
+//  Copyright © 2016年 Cherri Tech, Inc. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPDirect.
+FOUNDATION_EXPORT double TPDirectVersionNumber;
+
+//! Project version string for TPDirect.
+FOUNDATION_EXPORT const unsigned char TPDirectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPDirect/PublicHeader.h>
+
+#import <TPDirect/TPDSetup.h>
+#import <TPDirect/TPDCard.h>
+#import <TPDirect/TPDCardInfo.h>
+#import <TPDirect/TPDForm.h>
+#import <TPDirect/TPDStatus.h>
+#import <TPDirect/TPDApplePay.h>
+#import <TPDirect/TPDMerchant.h>
+#import <TPDirect/TPDConsumer.h>
+#import <TPDirect/TPDCart.h>
+#import <TPDirect/TPDPaymentItem.h>
+#import <TPDirect/TPDTransactionResult.h>
+#import <TPDirect/TPDCardValidationResult.h>
+#import <TPDirect/TPDLinePay.h>
+#import <TPDirect/TPDLinePayResult.h>
+
+
+
