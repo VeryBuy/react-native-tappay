9fbe4d644faeccf7d58a335a9fd680f942abf864
Dylan <dehao@verybuy.tw>
2021-09-23 09:18:22 +0800

    chore: Add the typescript and some setting

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import { UseTapPay, SetupArgs } from "./src/types/TapPayInstance";
-
-export function useTapPay(setupArgs: SetupArgs): UseTapPay;
-
-export type { default as TapPayInstance } from "./src/types/TapPayInstance";
