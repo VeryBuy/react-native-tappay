f4b801bec48065def49ecba52b33d519cecff568
Dylan <dehao@verybuy.tw>
2022-05-19 15:46:09 +0800

    feat(example): Remove import react

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -3,5 +3,7 @@ module.exports = {
   extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
   },
 };
--- example/babel.config.js
+++ example/babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    ['module:metro-react-native-babel-preset', { useTransformReactJSXExperimental: true }]
+  ],
+  plugins: [
+    ["@babel/plugin-transform-react-jsx", {
+      "runtime": "automatic"
+    }],
+  ],
 };
